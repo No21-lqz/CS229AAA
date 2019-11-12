4e0a5520520a3214778ee54f3199183f0e12e76c
Kent Eng <keng3@illinois.edu>
2019-11-12 15:43:17 -0700

    add

--- LIQIAN.py
+++ LIQIAN.py
@@ -1,2 +1,27 @@
 import numpy as np
-import kent
+import matplotlib.pyplot as plt
+import util
+# import ntlk
+import time
+
+def get_para(view, like, dislike, comment):
+    percen_like = like / view
+    percen_dislike = dislike / view
+    percen_comment = comment / view
+    parameter = (percen_like - percen_dislike) * percen_comment
+    return parameter
+
+def transform_time(date):
+    n = len(date)
+    for i in range(n):
+        date[i] = '20' + date[i]
+        print(date, type(date))
+    return date
+
+def main():
+    # view = util.load_spam_dataset('spam_train.tsv', 'views', add_intercept=False)
+    # like = util.load_spam_dataset('spam_train.tsv', 'likes', add_intercept=False)
+    # dislike = util.load_spam_dataset('spam_train.tsv', 'dislikes', add_intercept=False)
+    # comment = util.load_spam_dataset('spam_train.tsv', 'comment_count', add_intercept=False)
+    date = util.load_csv('USvideos.csv', label_col='trending_date', add_intercept=False)
+    transform_time(date)
--- util.py
+++ util.py
@@ -1,8 +1,10 @@
+import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import json
 
 
-def add_intercept(x):
+def add_intercept_fn(x):
     """Add intercept to matrix x.
 
     Args:
@@ -17,13 +19,12 @@ def add_intercept(x):
 
     return new_x
 
-
-def load_dataset(csv_path, label_col='y', add_intercept=False):
+def load_csv(csv_path, label_col, add_intercept=False):
     """Load dataset from a CSV file.
 
     Args:
          csv_path: Path to CSV file containing dataset.
-         label_col: Name of column to use as labels (should be 'y' or 't').
+         label_col: Name of column to use as labels (should be 'y' or 'l').
          add_intercept: Add an intercept entry to x-values.
 
     Returns:
@@ -31,25 +32,15 @@ def load_dataset(csv_path, label_col='y', add_intercept=False):
         ys: Numpy array of y-values (labels).
     """
 
-    def add_intercept_fn(x):
-        global add_intercept
-        return add_intercept(x)
-
-    # Validate label_col argument
-    allowed_label_cols = ('y', 't')
-    if label_col not in allowed_label_cols:
-        raise ValueError('Invalid label_col: {} (expected {})'
-                         .format(label_col, allowed_label_cols))
-
     # Load headers
-    with open(csv_path, 'r') as csv_fh:
+    with open(csv_path, encoding='gb18030', errors='ignore', newline='') as csv_fh:
         headers = csv_fh.readline().strip().split(',')
 
     # Load features and labels
     x_cols = [i for i in range(len(headers)) if headers[i].startswith('x')]
     l_cols = [i for i in range(len(headers)) if headers[i] == label_col]
-    inputs = np.loadtxt(csv_path, delimiter=',', skiprows=1, usecols=x_cols)
-    labels = np.loadtxt(csv_path, delimiter=',', skiprows=1, usecols=l_cols)
+    inputs = np.loadtxt(csv_path, delimiter=',', encoding='UTF-8', skiprows=1, usecols=x_cols)
+    labels = np.loadtxt(csv_path, delimiter=',', encoding='UTF-8', skiprows=1, usecols=l_cols)
 
     if inputs.ndim == 1:
         inputs = np.expand_dims(inputs, -1)
@@ -59,6 +50,28 @@ def load_dataset(csv_path, label_col='y', add_intercept=False):
 
     return inputs, labels
 
+def load_spam_dataset(tsv_path):
+    """Load the spam dataset from a TSV file
+
+    Args:
+         csv_path: Path to TSV file containing dataset.
+
+    Returns:
+        messages: A list of string values containing the text of each message.
+        labels: The binary labels (0 or 1) for each message. A 1 indicates spam.
+    """
+
+    messages = []
+    labels = []
+
+    with open(tsv_path, 'r', newline='', encoding='utf8') as tsv_file:
+        reader = csv.reader(tsv_file, delimiter='\t')
+
+        for label, message in reader:
+            messages.append(message)
+            labels.append(1 if label == 'spam' else 0)
+
+    return messages, np.array(labels)
 
 def plot(x, y, theta, save_path, correction=1.0):
     """Plot dataset and fitted logistic regression parameters.
@@ -68,7 +81,7 @@ def plot(x, y, theta, save_path, correction=1.0):
         y: Vector of labels in {0, 1}.
         theta: Vector of parameters for logistic regression model.
         save_path: Path to save the plot.
-        correction: Correction factor to apply, if any.
+        correction: Correction factor to apply (Problem 2(e) only).
     """
     # Plot dataset
     plt.figure()
@@ -77,13 +90,35 @@ def plot(x, y, theta, save_path, correction=1.0):
 
     # Plot decision boundary (found by solving for theta^T x = 0)
     x1 = np.arange(min(x[:, -2]), max(x[:, -2]), 0.01)
-    x2 = -(theta[0] / theta[2] + theta[1] / theta[2] * x1
-           + np.log((2 - correction) / correction) / theta[2])
+    x2 = -(theta[0] / theta[2] * correction + theta[1] / theta[2] * x1)
     plt.plot(x1, x2, c='red', linewidth=2)
-    plt.xlim(x[:, -2].min()-.1, x[:, -2].max()+.1)
-    plt.ylim(x[:, -1].min()-.1, x[:, -1].max()+.1)
 
     # Add labels and save to disk
     plt.xlabel('x1')
     plt.ylabel('x2')
     plt.savefig(save_path)
+
+
+def plot_contour(predict_fn):
+    """Plot a contour given the provided prediction function"""
+    x, y = np.meshgrid(np.linspace(-10, 10, num=20), np.linspace(-10, 10, num=20))
+    z = np.zeros(x.shape)
+
+    for i in range(x.shape[0]):
+        for j in range(y.shape[1]):
+            z[i, j] = predict_fn([x[i, j], y[i, j]])
+
+    plt.contourf(x, y, z, levels=[-float('inf'), 0, float('inf')], colors=['orange', 'cyan'])
+
+def plot_points(x, y):
+    """Plot some points where x are the coordinates and y is the label"""
+    x_one = x[y == 0, :]
+    x_two = x[y == 1, :]
+
+    plt.scatter(x_one[:,0], x_one[:,1], marker='x', color='red')
+    plt.scatter(x_two[:,0], x_two[:,1], marker='o', color='blue')
+
+def write_json(filename, value):
+    """Write the provided value as JSON to the given filename"""
+    with open(filename, 'w') as f:
+        json.dump(value, f)
