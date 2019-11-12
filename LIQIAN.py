722a66ed98b39ab60b041466cf1ef3ee26375cde
Kent Eng <keng3@illinois.edu>
2019-11-12 16:07:23 -0700

    loading data

--- LIQIAN.py
+++ LIQIAN.py
@@ -5,18 +5,19 @@ import util
 import time
 
 def get_para(view, like, dislike, comment):
+    """
+    :param view: number of view, int
+    :param like: number of like, int
+    :param dislike: number dislike, int
+    :param comment: number of comment, int
+    :return: parameter, float
+    """
     percen_like = like / view
     percen_dislike = dislike / view
     percen_comment = comment / view
     parameter = (percen_like - percen_dislike) * percen_comment
     return parameter
 
-def transform_time(date):
-    n = len(date)
-    for i in range(n):
-        date[i] = '20' + date[i]
-        print(date, type(date))
-    return date
 
 def main():
     # view = util.load_spam_dataset('spam_train.tsv', 'views', add_intercept=False)
@@ -24,4 +25,4 @@ def main():
     # dislike = util.load_spam_dataset('spam_train.tsv', 'dislikes', add_intercept=False)
     # comment = util.load_spam_dataset('spam_train.tsv', 'comment_count', add_intercept=False)
     date = util.load_csv('USvideos.csv', label_col='trending_date', add_intercept=False)
-    transform_time(date)
+main()
