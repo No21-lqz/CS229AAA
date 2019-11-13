e175e0ce368929249f7c79a266fac372b93e91cb
Kent Eng <keng3@illinois.edu>
2019-11-12 17:18:35 -0700

    loading number

--- kent.py
+++ kent.py
@@ -1,5 +1,7 @@
 import util
 import os
+import numpy as np
+import pandas as pd
 
 
 def load_predict_number_dataset(csv_path):
@@ -14,5 +16,20 @@ def load_predict_number_dataset(csv_path):
         dislike: Numpy array shape(n_examples, 1)
         comment: Numpy array shape(n_examples, 1)
     """
+    # Load headers
+    with open(csv_path, encoding='gb18030', errors='ignore', newline='') as csv_fh:
+        headers = csv_fh.readline().strip().split(',')
 
-#util.load_dataset('USvideos', add_intercept=False)
+    # Load features and labels
+    view_cols = [i for i in range(len(headers)) if headers[i] == 'views']
+    likes_cols = [i for i in range(len(headers)) if headers[i] == 'likes']
+    dislikes_cols = [i for i in range(len(headers)) if headers[i] == 'dislikes']
+    comment_cols = [i for i in range(len(headers)) if headers[i] == 'comment_count']
+    views = np.array(pd.read_csv(csv_path, usecols = view_cols)).ravel()
+    likes = np.array(pd.read_csv(csv_path, usecols = likes_cols)).ravel()
+    dislikes = np.array(pd.read_csv(csv_path, usecols = dislikes_cols)).ravel()
+    comment_count = np.array(pd.read_csv(csv_path, usecols = comment_cols)).ravel()
+
+    return views, likes, dislikes, comment_count
+
+views, likes, dislikes, comment_count=load_predict_number_dataset('last_trendingdate_train.csv')
