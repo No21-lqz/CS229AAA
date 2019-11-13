a74d2dac46a59d8332720ea3ecbfbcaa48022549
zhang <zhangliqian191@gmail.com>
2019-11-12 20:15:22 -0800

    Calculate the parameter defining whether it is hot or not

--- LIQIAN.py
+++ LIQIAN.py
@@ -5,16 +5,40 @@ import util
 import nltk
 import time
 
+
 def get_para(view, like, dislike, comment):
     """
-    :param view: number of view, int
-    :param like: number of like, int
-    :param dislike: number dislike, int
-    :param comment: number of comment, int
-    :return: parameter, float
+    :param view: number of view, NumPy array shape (n_examples, 1)
+    :param like: number of like, NumPy array shape (n_examples, 1)
+    :param dislike: number dislike, NumPy array shape (n_examples, 1)
+    :param comment: number of comment, NumPy array shape (n_examples, 1)
+    :return: parameter, NumPy array shape (n_examples, 1), float
     """
-    percen_like = like / view
-    percen_dislike = dislike / view
-    percen_comment = comment / view
-    parameter = (percen_like - percen_dislike) * percen_comment
-    return parameter
+    return (like - 1.5 * dislike) * comment / view
+
+def label(view, parameter, view_bar, para_bar):
+    """
+        Args:
+         view: number of view, NumPy array shape (n_examples, 1)
+         parameter: the enmotional trend of the reflects from viewers, NumPy array shape (n_examples, 1)
+         view_bar: number dislike, NumPy array shape (n_examples, 1)
+         para_bar: bars of parameters, a list (2,)
+
+    Returns:
+        label, NumPy array shape (n_examples, 1), int
+
+        """
+
+    label = np.zeros(view.shape())
+    n = len(view)
+    [bar1, bar2] = para_bar
+    for i in range(n):
+        if view[i] < view_bar:
+            label[i] = 0
+        elif parameter < bar1:
+            label[i] = 1
+        elif para_bar < bar2:
+            label[i] = 2
+        else:
+            label[i] = 3
+    return label
--- Prelimentary Design.py
+++ Prelimentary Design.py
@@ -1,4 +1,12 @@
 import math
-import utils
+import util
 import numpy as np
-import matlab.plot as plt
+import matplotlib.pyplot as plt
+import kent as kent
+import LIQIAN as zlq
+import lyp_preprocessing as lyp
+
+views, likes, dislikes, comment_count = kent.load_predict_number_dataset('last_trendingdate_test.csv')
+train_parameter = zlq.get_para(views, likes, dislikes, comment_count)
+view_bar, para_bar = 100000, [0, 300]
+label = zlq.label(views, train_parameter, view_bar, para_bar)
