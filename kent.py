2a501c8fde5d99ddcf7c913ab2f0c07bb0deb6cb
Kent Eng <keng3@illinois.edu>
2019-11-12 22:53:55 -0700

    list

--- kent.py
+++ kent.py
@@ -18,7 +18,7 @@ def load_predict_number_dataset(csv_path):
     """
     # Load headers
     with open(csv_path, encoding='gb18030', errors='ignore', newline='') as csv_fh:
-        headers = csv_fh.readline().strip().split(',')
+        headers = csv_fh    .readline().strip().split(',')
     # Load features and labels
     view_cols = [i for i in range(len(headers)) if headers[i] == 'views']
     likes_cols = [i for i in range(len(headers)) if headers[i] == 'likes']
@@ -31,4 +31,25 @@ def load_predict_number_dataset(csv_path):
 
     return views, likes, dislikes, comment_count
 
-views, likes, dislikes, comment_count=load_predict_number_dataset('last_trendingdate_train.csv')
+def load_number_dataset(csv_path, header):
+    """Load dataset for view, like, dislike, comment number.
+
+    Args:
+         csv_path: Path to CSV file containing dataset.
+         header: input the header of column (type: string)
+
+    Returns:
+        view: NumPy array shape (n_examples, 1)
+        like: NumPy array shape (n_examples, 1)
+        dislike: Numpy array shape(n_examples, 1)
+        comment: Numpy array shape(n_examples, 1)
+    """
+    # Load headers
+    with open(csv_path, encoding='gb18030', errors='ignore', newline='') as csv_fh:
+        headers = csv_fh    .readline().strip().split(',')
+    # Load features and labels
+    cols = [i for i in range(len(headers)) if headers[i] == header]
+    data = np.array(pd.read_csv(csv_path, usecols = cols)).ravel()
+
+
+    return data
