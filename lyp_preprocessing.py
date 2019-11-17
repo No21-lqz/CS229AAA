b428898066afd9d092c700a4834c7ee2a4c258c4
liyuping <yplstudy@gmail.com>
2019-11-16 22:02:50 -0800

    add a lot of things

--- lyp_preprocessing.py
+++ lyp_preprocessing.py
@@ -7,6 +7,7 @@ import time
 import LIQIAN
 from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
 import csv
 
 
@@ -56,5 +57,103 @@ def create_3d(a, b, c, d, e):
         train_p[i, :, :] = np.array([a[i], b[i], c[i], d[i], e[i]]).transpose()
     return train_p
 
-def :
+def trainlogsitic(X_train, Y_train):
+    n, m, z = X_train.shape
+    theta = np.zeros([z, 1])
+    new_x = np.zeros([n * 4, 5])
+    new_x = X_train.reshape([n * 4, 5])
+    new_y = Y_train.reshape([n * 4, 1]).ravel()
+    print(new_x)
+    print(new_y)
+    model = LogisticRegression(max_iter=10000, solver='sag', tol=1e-6)
+    result = model.fit(new_x, new_y)
+    para = model.coef_
+    print(para)
+    return para.ravel()
 
+def trainsoftmax(X_train, Y_train):
+    '''
+    itera = 0
+    ll = 1
+    pre_ll = 0
+    while itera < max_iter and abs(ll - pre_ll) > 1e-5:
+        sig= 0
+        for i in range(z):
+            sum = 0
+            for j in range(n):
+                for k in range(m):
+                    if Y_train[i, k] == 1:
+                        mul = np.matmul(X_train[j, k, :], theta)
+                        sum += (-1) * (1 - sigmoid(mul)) * X_train[j, k, i] + theta[i]
+            theta[i] += alpha * sum
+        for i in range(n):
+            mul = 0
+            for j in range(m):
+                if Y_train[i, j] == 1:
+                    mul += np.matmul(X_train[i, j, :], theta)
+            sig += (-1) * np.log(sigmoid(mul)) + 1/2 * lamda * np.linalg.norm(theta)
+        sig = sig / n
+        pre_ll = ll
+        ll = sig
+        itera += 1
+        print('iteration: {:05d}', itera)
+        print('ll: {:.6f}', ll)'''
+
+    return theta
+
+def normalization(target):
+    """
+    :param target: a n * 2 matrix with intercept
+    :return: intercept is unchanged, and the variable in the other column is normalized (return n * 2 matrix)
+    """
+    max_t = max(target[:, 1])
+    min_t = min(target[:, 1])
+    print(max_t)
+    print(min_t)
+    n = target.shape[0]
+    normal_target = np.zeros([n, 2])
+    for i in range(n):
+        normal_target[i, 0] = target[i, 0]
+        normal_target[i, 1] = (target[i, 1] - min_t + 1) / (max_t - min_t)
+    return normal_target
+
+def predict_pro(predict_p, theta):
+    n, m, z = predict_p.shape
+    pro = np.zeros([n, 1])
+    for i in range(n):
+        pro[i] = np.matmul(predict_p[i, :, :], theta).argmax(axis=0)
+    return pro
+
+def predict(y):
+    """
+
+    :param y: n * 4 matrix needed to be labelled
+    :return: return predicted label
+    """
+    n = y.shape[0]
+    predict_label = np.zeros(n)
+    predict_label = y.argmax(axis=1)
+    return predict_label
+
+def accuracy(y, y_predict):
+    """
+
+    :param : y is a original label matrix, y_predict is a predicted label matrix (n * 1)
+    :return: accuracy
+    """
+    n = y.shape[0]
+    count = 0
+    for i in range(n):
+        if y[i] == y_predict[i]:
+            count += 1
+    acc = count / n
+    return
+
+def crossentropy(y_predict, y_label):
+    n, m = y_predict.shape
+    sum = 0
+    for i in range(n):
+        for j in range(m):
+            if y_label[i, j] == 1:
+                sum += -np.log(y_predict[i, j])
+    return sum
